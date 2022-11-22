7cd634719f5a4d947e605e80b8c9027bc79835e8
Javier Madrazo Ortiz <61298004+Javier-debug@users.noreply.github.com>
2022-11-22 12:41:52 -0600

    Version 1.0.0

---
+++ HoloLensTest/Assets/MRTK/Scripts/dashboards.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dashboards : MonoBehaviour
+{
+    public string url;
+    public Renderer renderer;
+    public Image renderer2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadFromLikeCoroutine());
+        renderer.material.color = Color.green;
+    }
+
+    private IEnumerator LoadFromLikeCoroutine()
+    {
+        WWW wwwLoader = new WWW(url);
+        yield return wwwLoader;
+
+        renderer.material.color = Color.white;
+        renderer.material.mainTexture = wwwLoader.texture;
+        //renderer2.sprite = Sprite.Create(wwwLoader.texture, new Rect(0, 0, wwwLoader.texture.width, wwwLoader.texture.height), Vector2.one / 2);
+    }
+}
---
+++ HoloLensTest/Assets/Scripts/LoadImage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
+
+public class LoadImage : MonoBehaviour
+{
+    public string url = "";
+    public Renderer renderer;
+
+    void Start()
+    {
+
+    }
+}
