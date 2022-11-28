0cfdde6b0e0157eee30af0cee5586be83f31e55d
Javier Madrazo Ortiz <61298004+Javier-debug@users.noreply.github.com>
2022-11-28 13:05:34 -0600

    HandMenu

---
+++ HoloLensTest/Assets/MRTK/Scripts/HandMenuOption.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HandMenuOption : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro title;
+    public GameObject menu;
+    public string section;
+    void Start()
+    {
+        
+    }
+
+    public void showMenu()
+    {
+        menu.SetActive(true);
+        title.text = section;
+    }
+}
