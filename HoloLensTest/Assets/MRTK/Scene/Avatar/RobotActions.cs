a01d1164976eb783c12324dc6bd2b08b81f239f1
Javier Madrazo Ortiz <61298004+Javier-debug@users.noreply.github.com>
2022-12-09 08:17:34 -0600

    First animation Intro

---
+++ HoloLensTest/Assets/MRTK/Scene/Avatar/RobotActions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotActions : MonoBehaviour
+{
+    public GameObject dashboardDaños;
+    public GameObject dashboardSiniestros;
+    public GameObject dashboardCostos;
+    public GameObject dashboardRefacciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(appearDashboards());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //dashboards.SetActive(false);
+    }
+
+    IEnumerator appearDashboards()
+    {
+        yield return new WaitForSeconds(4);
+        dashboardRefacciones.SetActive(true);
+        yield return new WaitForSeconds(4);
+        dashboardDaños.SetActive(true);
+        yield return new WaitForSeconds(4);
+        dashboardCostos.SetActive(true);
+        yield return new WaitForSeconds(4);
+        dashboardSiniestros.SetActive(true);
+        
+    }
+
+
+}
